6b426900115c9428418ad5d2d22b4385d8fa85f9
HabiburRahmanAnik <habib264521@gmail.com>
2022-03-11 19:16:41 +0600

    impletement locale date

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2022-03-08T16:01:17.194Z',
+    '2022-03-09T16:36:17.929Z',
+    '2022-03-10T10:51:36.790Z',
+  ],
+  currency: 'BDT',
+  locale: 'en-BD', // de-DE
 };
 
 const account2 = {
@@ -18,6 +30,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2022-03-8T14:43:26.374Z',
+    '2022-03-9T18:49:59.371Z',
+    '2022-03-10T12:01:20.894Z',
+  ],
+  currency: 'BDT',
+  locale: 'bn-BD',
 };
 
 const account3 = {
@@ -25,6 +49,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2022-03-10T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -32,6 +68,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: '',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -67,15 +115,33 @@ containerApp.style.opacity = 0;
 
 // FUNCTION
 
+const dateFormatter = function (date, locale) {
+  const formater = (date1, date2) =>
+    Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = formater(new Date(), date);
+
+  if (daysPassed === 1) return 'Today';
+  if (daysPassed === 2) return 'Yestarday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
 // display movements
-const displayMovements = function (acc) {
+const displayMovements = function (accs) {
   containerMovements.textContent = '';
 
-  acc.movements.forEach(acc => {
+  accs.movements.forEach((acc, i) => {
     const movs = acc > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(accs.movementsDates[i]);
+    const formateDate = dateFormatter(date, accs.locale);
+
     const html = `
     <div class="movements__row">
           <div class="movements__type movements__type--${movs}">2 ${movs}</div>
+          <div class="movements__date">${formateDate}</div>
           <div class="movements__value">${acc}</div>
         </div>
     `;
@@ -131,7 +197,9 @@ const createUsername = function (accounts) {
 createUsername(accounts);
 
 //which account is logged in
-let currentAccount;
+let currentAccount = account1;
+containerApp.style.opacity = 100;
+updateUI(currentAccount);
 
 // LOGIN
 btnLogin.addEventListener('click', function (e) {
@@ -146,6 +214,22 @@ btnLogin.addEventListener('click', function (e) {
     labelWelcome.innerText = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
+
+    const now = new Date();
+
+    const opetions = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'long',
+    };
+    const dateFormate = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      opetions
+    ).format(now);
+    labelDate.textContent = dateFormate;
 
     // show UI
     containerApp.style.opacity = 100;
@@ -201,7 +285,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    +inputClosePin.value === currentAccount.value
+    +inputClosePin.value === currentAccount.pin
   ) {
     const deleteAccountIndex = accounts.findIndex(
       acc =>
